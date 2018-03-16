dc770b44bc71e476cce2410bece8de884dcac5d2
Shawn <2273904129@qq.com>
2018-03-16 11:55:37 +0800

    修改一些bug

--- application/views/pages/editor.php
+++ application/views/pages/editor.php
@@ -4,10 +4,6 @@
 
 <!--style给定宽度可以影响编辑器的最终宽度-->
 <script type="text/plain" id="myEditor" style="width:800px;height:240px;">
-    <p>这里我可以写一些输入提示</p>
+    <p>请管理员不要上传太多图片</p>
 </script>
 
-<script type="text/javascript">
-    //实例化编辑器，保证id与ui对应，custom_editor文件中包含相应api及实例化
-    //var um = UM.getEditor('myEditor');
-</script>
